f55c9bf0a97725e92fcbfa4e6099e4b3d239b66f
inpercima <inpercima@gmail.com>
2019-11-19 11:34:49 +0100

    Use Junit5

---
+++ src/test/java/net/inpercima/restapi/service/RestApiServiceTest.java
@@ -0,0 +1,17 @@
+package net.inpercima.restapi.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RestApiServiceTest {
+
+    private final RestApiService restApiService = new RestApiService();
+
+    @Test
+    public void getForObject() {
+        final String url = "https://httpbin.org/get";
+        final Object object = restApiService.getForObject(url, "application/json", "", Object.class).getBody();
+        Assertions.assertNotNull(object);
+    }
+
+}
